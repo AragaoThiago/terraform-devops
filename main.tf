5ee3a16caae9c657bbedff995acfb350f7666969
thiago aragao <devops@MacBook-Air-de-thiago.local>
2024-01-28 18:40:54 -0300

    primeiros

--- main.tf
+++ main.tf
@@ -347,10 +347,10 @@ resource "aws_lb_target_group" "app" {
     path                = "/actuator/health"
     port                = 80
     matcher             = 200
-    interval            = 10
-    timeout             = 5
-    healthy_threshold   = 2
-    unhealthy_threshold = 3
+    interval            = 300
+    timeout             = 120
+    healthy_threshold   = 5
+    unhealthy_threshold = 8
   }
 }
 
